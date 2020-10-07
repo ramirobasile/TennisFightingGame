9e8c9a945aad5d2bb9732233ad3dcbc0e259f8ed
Ramiro Basile <ramirobasile1@gmail.com>
2020-10-07 00:00:10 -0300

    Bar now fills up with slight delay

--- Game/Components/UI/Bar.cs
+++ Game/Components/UI/Bar.cs
@@ -5,11 +5,15 @@ namespace TennisFightingGame.UI
 {
     public struct Bar
     {
+        private const float FillSpeed = 12;
+
         private readonly Texture2D backgroundTexture;
         private readonly Texture2D fillTexture;
         public readonly Rectangle rectangle;
         private readonly int direction;
         private readonly Color color;
+        
+        private Rectangle fillRectangle;
 
         public Bar(Texture2D backgroundTexture, Texture2D fillTexture,
         	Rectangle rectangle, Color color, int direction = 1)
@@ -19,19 +23,25 @@ namespace TennisFightingGame.UI
             this.rectangle = rectangle;
             this.direction = direction;
             this.color = color;
+
+            fillRectangle = new Rectangle();
         }
 
-        public void Draw(SpriteBatch spriteBatch, float fill)
+        public void Draw(SpriteBatch spriteBatch, float newFill)
         {
-            Rectangle fillRectangle;
+            float fill = (int)MathHelper.Lerp(
+                fillRectangle.Width, 
+                rectangle.Width * newFill, 
+                TennisFightingGame.DeltaTime * FillSpeed) + 1;
+
             if (direction == 1)
             {
-                fillRectangle = new Rectangle(rectangle.X, rectangle.Y, (int) (rectangle.Width * fill), rectangle.Height);
+                fillRectangle = new Rectangle(rectangle.X, rectangle.Y, (int)fill, rectangle.Height);
             }
             else
             {
-                fillRectangle = new Rectangle(rectangle.X + rectangle.Width - (int) (rectangle.Width * fill), rectangle.Y,
-                    (int) (rectangle.Width * fill), rectangle.Height);
+                fillRectangle = new Rectangle(rectangle.X + rectangle.Width - (int)fill, rectangle.Y,
+                    (int)fill, rectangle.Height);
             }
 
             spriteBatch.Draw(backgroundTexture, rectangle, Color.Gray); // colors are WIP
